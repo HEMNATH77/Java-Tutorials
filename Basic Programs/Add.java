ca941aab02af629a2e3ce82e312ee8d9f5bc0c36
Hemnath M <146712643+HEMNATH77@users.noreply.github.com>
2025-01-20 23:27:08 +0530

    Add files via upload

---
+++ Basic Programs/Add.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+class Add{
+public static void main(String[]args){
+
+Scanner Sc = new Scanner(System.in);
+int a = Sc.nextInt();
+int b =Sc.nextInt();
+int c = a+b;
+
+System.out.println("Sum : "+c);
+}
+}
+
+
+
