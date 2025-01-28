b867f5f028c41413181a64556da56d235966c325
Hemnath M <146712643+HEMNATH77@users.noreply.github.com>
2025-01-28 21:55:15 +0530

    Add files via upload

---
+++ Special Programs/Comp.java
@@ -0,0 +1,25 @@
+class Comp {
+
+public static void main(String[]args) {
+
+char letter1 = 'A';
+char letter2 = 'B';
+
+
+System.out.println(letter1 + "<" + letter2 + "=" + (letter1 < letter2));
+
+System.out.println(letter1 + "<=" + letter2 + "=" + (letter1 <= letter2));
+
+System.out.println(letter1 + "<" + letter2 + "=" + (letter1 > letter2));
+
+System.out.println(letter1 + "<=" + letter2 + "=" + (letter1 >= letter2));
+
+System.out.println(letter1 + "!=" + letter2 + "=" + (letter1 == letter2));
+
+System.out.println(letter1 + "==" + letter2 + "=" + (letter1 != letter2));
+
+
+
+
+}
+}
