966320412d12bbea09147edcb9f4733652474ad8
Hemnath M <146712643+HEMNATH77@users.noreply.github.com>
2025-04-18 20:52:12 +0530

    Create String1.java

---
+++ Strings/String Comparision /String1.java
@@ -0,0 +1,14 @@
+class String1{
+public static void main(String[] args){
+
+ String s1 = new String("Rama");
+ String s2 = new String("Rama");
+
+ if(s1.equals(s2)){
+   System.out.println("Strings are equal");
+ }
+ else{
+   System.out.println("Strings are unequal");
+}
+}
+}
